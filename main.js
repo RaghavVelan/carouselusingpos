b09072bb944e4361f36ba9e2429f2eb76a14806f
raghavvelandl <raghavvelandl@gmail.com>
2024-12-11 04:14:02 +0000

    implemented button control for the carousel

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const cardsContainer = document.querySelector(".cardContainer")
 const expandBtn = document.querySelector(".expandBtn")
-// const prevBtn = document.querySelector(".PrevBtn")
-// const nextBtn = document.querySelector(".NextBtn")
+const prevBtn = document.querySelector(".PrevBtn")
+const nextBtn = document.querySelector(".NextBtn")
+
+let currentIndex = 0;
 
 stack();
 
@@ -22,7 +24,8 @@ function carousel(){
     const cards = document.querySelectorAll(".card");
     cards.forEach((card, index) => {
         card.style.zIndex = 0;
-        card.style.left = `${(index * 300) + (16 * (index + 1))}px`;
+        // card.style.left = `${(index * 300) + (16 * (index + 1))}px`;
+        card.style.left = `${((index * 300) + (16 * (index + 1))) - (316 * currentIndex)}px`; 
     })
 }
 
@@ -34,3 +37,19 @@ function stack(){
         card.style.left = `calc(50% + ${index * 1}rem)`; 
     });
 }
+
+nextBtn.addEventListener("click", () => {
+    const cards = document.querySelectorAll(".card");
+    if(currentIndex < cards.length - 1){
+        currentIndex++;
+        carousel();
+    }
+})
+
+prevBtn.addEventListener("click", () => {
+    const cards = document.querySelectorAll(".card");
+    if(currentIndex > 0){
+        currentIndex--;
+        carousel();
+    }
+})
